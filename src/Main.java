cde9fe183415f0d60bed9964c32c664c76ccdb81
damianBlock <damianfrt25@gmail.com>
2024-03-06 20:36:51 -0300

    Ejercicios de Academlo

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+
+        /* Ejercicio 1: Creando variables
+
+        Crea variables para guardar los siguientes datos de una persona:
+        Nombre
+        Apellidos
+        Edad
+        Ciudad
+
+        Asignar valores a las variables creadas. */
+
+        var Nombre = "Damian";
+        var Apellidos = "Juarez";
+        var edad = 34;
+        var ciudad = "San Miguel de Tucumán";
+
+
+        System.out.println(Nombre + " " + Apellidos + " " + edad + " " + ciudad );
+    }
+}
