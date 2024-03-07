26c98ecf57e714c280734b2a76dacac5021c7800
damianBlock <damianfrt25@gmail.com>
2024-03-06 21:13:47 -0300

    Segundo ejercicio de arreglos

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -16,7 +19,21 @@ public class Main {
         var edad = 34;
         var ciudad = "San Miguel de Tucumán";
 
-
         System.out.println(Nombre + " " + Apellidos + " " + edad + " " + ciudad );
+
+
+
+        /* Ejercicio 2: Sumando los valores de un arreglo
+
+        Crear un array con 3 valores numericos y sumar todos esos valores accedendolos mediante las posiciones. */
+
+        int [] arreglo = {10, 22, 34};
+
+        int suma =  arreglo[0]  + arreglo[1] + arreglo[2];
+
+        System.out.println(suma);
+
+
+
     }
 }
